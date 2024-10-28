433bdd94ce660e189c6c88ada8f1c7314082a65b
mal1on <lyubo.malinov@gmail.com>
2024-10-28 10:14:53 +0200

    Sentences and characters count upgraded

--- Text Analyzer/main.py
+++ Text Analyzer/main.py
@@ -1,20 +1,24 @@
+import re
+from string import punctuation
 from collections import Counter
+
 
 
 def main():
     '''Takes a block of text provided by the user, analyzes it, and
      provides useful statistics about the text to the user.'''
 
-    text = input('Enter text to analyze: ')
+    # text = input('Enter text to analyze: ')
+    text = "Musk, in particular, has a relationship with Erdoğan, whom The Atlantic described as making Turkey authoritarian again. The pair met in 2017, The Washington Post reported, and SpaceX signed a deal with Turkey in 2021. The next year at the World Cup, Musk and Erdoğan shook hands, the Post reported. Just last month, Insider reported, SpaceX assisted Turkey in a satellite launch, and critics believe that Musk's dealings with Erdoğan may have prompted Twitter's easy compliance with the most recent takedown request, seemingly intended to silence Erdoğan's detractors ahead of his re-election bid."
 
     words = Counter(text.split())
-    sentences = text.split('.')[:-1]
-    chars = len(text.replace(' ', ''))
+    sentences = len(re.findall('[.!?;]+ ', text)) + 1
+    chars = len(text.translate(str.maketrans('', '', punctuation + ' ')))
     most_freq = ['"' + t[0].lower() + '"' for t in words.most_common(3)]
     avg_word_l = chars // len(text.split())
-    avg_sent_l = len(text.split()) // len(sentences)
+    avg_sent_l = len(text.split()) // sentences
 
-    print(f'There are {len(words)} words, {len(sentences)} sentences and \
+    print(f'There are {len(words)} words, {sentences} sentences and \
 {chars} non-blank characters in the text entered.')
     print(f'The three most frequent words in the text are \
 {", ".join(most_freq[:-1])} and {most_freq[-1]}.')
