f364f212a9861eec7406a19f80c08155b79fa210
mal1on <lyubo.malinov@gmail.com>
2024-10-29 10:22:20 +0200

    Add view_tasks method

--- Task Manager/main.py
+++ Task Manager/main.py
@@ -17,18 +17,27 @@ class ToDoList:
     def add_task(self):
         description = input("Enter the task description: ")
         self.tasks.append(Task(description))
+        print(f'Task "{description}" added to the To-Do list.')
 
     def complete_task(self):
-        # self.view_tasks()
+        self.view_tasks()
         task = int(input('Enter the task number to mark as completed: '))
-        while task not in range(len(self.tasks) - 1):
+        while task not in range(1, len(self.tasks) + 1):
             print('Please enter existing task number')
-            # self.view_tasks()
+            self.view_tasks()
             task = int(input('Enter the task number to mark as completed: '))
-        self.tasks[task].mark_completed()
+        self.tasks[task - 1].mark_completed()
+
+    def view_tasks(self):
+        print('To-Do List:')
+        for n, task in enumerate(self.tasks):
+            if task.is_completed:
+                print(f'{n + 1}. {task.description} [✔]')
+            else:
+                print(f'{n + 1}. {task.description} [❌]')
 
 
 
 test = ToDoList()
 test.add_task()
-test.complete_task
+test.complete_task()
