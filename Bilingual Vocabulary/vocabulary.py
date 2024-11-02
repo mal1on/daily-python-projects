a79dab8d6a35b5414d764f49801e44f7a2c8b1b6
mal1on <lyubo.malinov@gmail.com>
2024-11-02 16:49:20 +0200

    Solution updated with new print statements

--- Bilingual Vocabulary/vocabulary.py
+++ Bilingual Vocabulary/vocabulary.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 
 def vocabulary():
@@ -10,15 +11,24 @@ def vocabulary():
     words = [('Language 1', 'Language 2')]
     while True:
         word = input('Enter a word in language 1 (or type "done" to finish): ')
-        if word != 'done':
+        if word.lower() != 'done':
             trans = input(f'Enter the translation of "{word}" in language 2: ')
             words.append((word.lower(), trans.lower()))
+            print(
+                f'"{word.lower()}" with translation "{trans.lower()}" has been added')
         else:
             break
 
     with open('data.csv', 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerows(words)
+
+    dash = '-' * os.get_terminal_size().columns
+    print('Bilingual vocabulary created: ')
+    print(dash)
+    for word, trans in words[1:]:
+        print(f'{word}: {trans}')
+    print(dash)
 
 
 if __name__ == '__main__':
