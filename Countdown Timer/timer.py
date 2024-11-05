b9a583d119093459468b9ce447fe73dfeef89d6b
mal1on <lyubo.malinov@gmail.com>
2024-11-05 22:48:57 +0200

    End character replaced with carriage return

--- Countdown Timer/timer.py
+++ Countdown Timer/timer.py
@@ -7,7 +7,7 @@ def timer():
 
     while secs:
         m, s = divmod(secs, 60)
-        print(f'{m:02d}:{s:02d}\n')
+        print(f'{m:02d}:{s:02d}', end='\r')
         time.sleep(1)
         secs -= 1
     else:
