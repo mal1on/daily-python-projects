b68ca53489eadf8140d0c7b34a6c39ae42a6d41d
mal1on <lyubo.malinov@gmail.com>
2024-11-26 15:41:22 +0200

    Program file created

---
+++ Advanced Person Name Generator/gen.py
@@ -0,0 +1,12 @@
+def gen():
+    '''
+    The program starts by showing the user a menu. The user enters a
+    number (e.g., 2 to add a new name to the text file) and then
+    proceeds with the instructions (e.g., adding a name such as Wimbly).
+    The program adds that new name to the names.txt file and then shows
+    the menu again. In addition to adding new names, the user can also
+    display the total number of names, and select a random name from
+    the text file.
+    '''
+
+
