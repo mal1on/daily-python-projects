2a1668e8c1327f7a469f8a51141ee601a8904b20
mal1on <lyubo.malinov@gmail.com>
2024-11-26 16:11:50 +0200

    First working version

--- Advanced Person Name Generator/gen.py
+++ Advanced Person Name Generator/gen.py
@@ -1,3 +1,6 @@
+import random
+
+
 def gen():
     '''
     The program starts by showing the user a menu. The user enters a
@@ -8,5 +11,34 @@ def gen():
     display the total number of names, and select a random name from
     the text file.
     '''
+    while True:
+        print('Please select an option: ')
+        print('1. Show the total number of names')
+        print('2. Add new name')
+        print('3. Select a random name')
+        print('4. Exit')
+        choice = int(input('Enter your choice (1-4): '))
+        match choice:
+            case 1:
+                with open('names.txt') as f:
+                    print(
+                        f'The total number of names is: {len(f.readlines())}\n')
+            case 2:
+                name = input('Enter the new name to add: ').capitalize()
+                with open('names.txt', 'a') as f:
+                    f.write(name + '\n')
+                print(f'{name} has been added to the file\n')
+            case 3:
+                with open('names.txt') as f:
+                    names = f.readlines()
+                    name = random.choice(names)
+                print(f'Selected name is: {name}')
+            case 4:
+                print('Exiting...')
+                break
+            case _:
+                print('Please enter valid choice (1-4)\n')
 
 
+if __name__ == '__main__':
+    gen()
