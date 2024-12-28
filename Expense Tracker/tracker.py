2eafa99497100c5829f524a5a84743bd29f28904
mal1on <lyubo.malinov@gmail.com>
2024-12-28 13:31:37 +0200

    Menu options 1 and 5 added

--- Expense Tracker/tracker.py
+++ Expense Tracker/tracker.py
@@ -1,8 +1,13 @@
+import os
+from datetime import datetime
+
+
 '''
 Command-line app to track daily expenses. The app allows users to add
 expenses, view a summary of all expenses, and calculate total spending
 for a given period.
 '''
+
 
 def menu():
     print('\nWelcome to the Expense Tracker!')
@@ -11,10 +16,52 @@ def menu():
     print('3. Search expense by date')
     print('4. Calculate total spending')
     print('5. Exit')
-    choice = int(input('Choose an option: '))
+    while True:
+        try:
+            choice = int(input('Choose an option: '))
+            if choice in range(1, 6):
+                break
+            else:
+                print('Choose available option (1-5).')
+        except ValueError:
+            print('Choose available option (1-5).')
 
     return choice
 
-a = menu()
-print('\ntest\n')
-print(a)
+
+def tracker():
+
+    with open('expenses.txt', 'a+') as file:
+        file.seek(0)
+        expenses = file.readlines()
+        print(expenses)
+        while True:
+            option = menu()
+            match option:
+                case 1:
+                    while True:
+                        date = input('Enter date (YYYY-MM-DD): ')
+                        try:
+                            date_o = datetime.strptime(date, '%Y-%m-%d')
+                            break
+                        except ValueError:
+                            print('Enter correct date (YYYY-MM-DD)')
+
+                    date = datetime.strftime(date_o, '%Y-%m-%d')
+                    cat = input('Enter category: ')
+                    desc = input('Enter description: ')
+                    while True:
+                        try:
+                            amount = float(input('Enter amount: '))
+                            break
+                        except ValueError:
+                            print('Enter correct amount (int or float).')
+                    expenses.append(f'{date},{cat},{desc},{amount}')
+                    file.write(f'\n{date},{cat},{desc},{amount}')
+                    print('Expense added successfully!')
+                case 5:
+                    print('Exiting...')
+                    break
+
+
+tracker()
